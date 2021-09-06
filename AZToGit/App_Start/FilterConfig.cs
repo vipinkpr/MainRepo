4447f83e432f08689289ee8331ab577cda71621e
vipinkpr <vipinkpr@hotmail.com>
2021-09-06 15:53:15 +0530

    abcd

---
+++ AZToGit/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AZToGit
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
